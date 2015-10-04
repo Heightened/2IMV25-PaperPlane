02654ee2b459f7e024e4033ac862eb98bc1bad06
noway <noway@noway.noway>
2015-10-04 15:25:40 +0200

    Testing git/unity combo with prototype

---
+++ PAPERPLANE_UNITY/Assets/MainBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MainBehavior : MonoBehaviour {
+
+	bool chase = false;
+	private int delay = 90;
+	// Use this for initialization
+	void Start () {
+		if (delay > SceneManagerScript.historySize-1) {
+			//crap
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (chase) {
+			Vector3 diff = SceneManagerScript.planePosition - transform.position;
+			transform.position += diff / 5;
+		} else {
+			int delayedPos = SceneManagerScript.historyPointer - delay;
+			if (delayedPos < 0) delayedPos += SceneManagerScript.historySize;
+			transform.position = SceneManagerScript.positionHistory[delayedPos];
+		}
+	}
+}
---
+++ PAPERPLANE_UNITY/Assets/SceneManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SceneManagerScript : MonoBehaviour {
+
+	public static Vector3 planePosition;
+	public static Vector3[] positionHistory;
+
+	public static int historySize = 100;
+	public static int historyPointer = 0;
+
+	private Plane tracePlane = new Plane(new Vector3(0,0,-1), new Vector3(0,0,0));
+	// Use this for initialization
+	void Start () {
+		positionHistory = new Vector3[historySize];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		float distance = 0f;
+		if (tracePlane.Raycast (ray, out distance)) {
+			planePosition = ray.GetPoint (distance);
+			positionHistory[historyPointer] = planePosition;
+			historyPointer++;
+			historyPointer %= historySize;
+		}
+	}
+}
